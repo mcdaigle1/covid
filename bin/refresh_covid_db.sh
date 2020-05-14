cde517d64d3e35472cb0722c193dac617b598793
Mike Daigle <mike.daigle@renttrack.com>
2020-05-13 21:13:08 -0700

    point main refresh script to new code

--- bin/refresh_covid_db.sh
+++ bin/refresh_covid_db.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-curl http://localhost:8086/query \
-  -d db=covid \
-  -d q='drop measurement "daily_deaths"'
+#curl http://localhost:8086/query \
+#  -d db=covid \
+#  -d q='drop measurement "daily_deaths"'
+#
+#curl http://localhost:8086/query \
+#  -d db=covid \
+#  -d q='drop measurement "state_data"'
+#
+#curl http://localhost:8086/query \
+#  -d db=covid \
+#  -d q='drop measurement "trend_daily_deaths"'
 
-curl http://localhost:8086/query \
-  -d db=covid \
-  -d q='drop measurement "state_data"'
-
-curl http://localhost:8086/query \
-  -d db=covid \
-  -d q='drop measurement "trend_daily_deaths"'
-
-/home/mdaigle/covid/src/initial_upload.py
+/home/mdaigle/covid/src/refresh_state_data.py
+/home/mdaigle/covid/src/refresh_state_mortality.py
+/home/mdaigle/covid/src/refresh_state_trends.py
