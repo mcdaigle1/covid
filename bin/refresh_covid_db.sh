b5fe3d8f9bc0d9c9d5a125eb619dedb2e438c175
Mike Daigle <mike.daigle@renttrack.com>
2020-05-13 23:14:05 -0700

    add seven day trend to nightly db refresh

--- bin/refresh_covid_db.sh
+++ bin/refresh_covid_db.sh
@@ -15,3 +15,4 @@
 /home/mdaigle/covid/src/refresh_state_data.py
 /home/mdaigle/covid/src/refresh_state_mortality.py
 /home/mdaigle/covid/src/refresh_state_trends.py
+/home/mdaigle/covid/src/refresh_state_trends_7_days.py
