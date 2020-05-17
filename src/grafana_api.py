f5b6cde00b3b0fc740dd339891343ece85aee1cb
Mike Daigle <mike.daigle@renttrack.com>
2020-05-17 09:05:57 -0700

    more grafana testing

--- src/grafana_api.py
+++ src/grafana_api.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python3
 
-#import requests
+import requests
 
 class GrafanaApi:
 
     GRAFANA_API_LOCATION = "/etc/covid/grafana_api.cfg"
-    grafana_api = ""
+    GRAFANA_API_URL = "http://localhost:3000/grafana/api/"
+
+    grafana_api_key = ""
 
     def __init__(self):
         f = open(GrafanaApi.GRAFANA_API_LOCATION, "r")
-        grafana_api = f.read()
-        print("Grafana API: " + grafana_api)
+        grafana_api_key = f.read()
 
+    def getDashByUid(self, uid):
+        uid = "fH0__8eZk"
+
+        url = GrafanaApi.GRAFANA_API_URL + "dashboards/home"
+        headers = {'Authorization': 'Bearer ' + grafana_api_key}
+
+        print("query grafana: " + url)
+        r = requests.get(url, headers=headers)
+        print(r)
 
