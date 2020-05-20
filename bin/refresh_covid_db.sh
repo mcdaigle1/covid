2ef1925b75fa610392268c838ea22229feefad19
Mike Daigle <mike.daigle@renttrack.com>
2020-05-19 22:22:53 -0700

    add avg data to cron script

--- bin/refresh_covid_db.sh
+++ bin/refresh_covid_db.sh
@@ -15,5 +15,6 @@
 /home/mdaigle/covid/src/refresh_state_data.py
 /home/mdaigle/covid/src/refresh_state_mortality.py
 /home/mdaigle/covid/src/refresh_state_trends.py
-/home/mdaigle/covid/src/refresh_state_trends_7_days.py
+#/home/mdaigle/covid/src/refresh_state_trends_7_days.py
 /home/mdaigle/covid/src/refresh_state_rank_dpm.py
+/home/mdaigle/covid/src/refresh_state_avg_7_days.py
